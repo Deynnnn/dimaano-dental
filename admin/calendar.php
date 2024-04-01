fd85cd8450dad2d94e045750880877929e9b1adc
Deynnn <rhoepas2002@gmail.com>
2024-04-01 15:22:43 +0800

    calendar on admin

---
+++ admin/calendar.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require('includes/links.php')?>
+    <link rel="stylesheet" href="../fullcalendar/lib/main.min.css">
+    <script src="../js/jquery-3.6.0.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../fullcalendar/lib/main.min.js"></script>
+    <title>DentaKlyn - Calendar</title>
+</head>
+<body class="bg-light">
+    <style>
+        :root {
+            --bs-success-rgb: 71, 222, 152 !important;
+        }
+ 
+        html,
+        body {
+            height: 100%;
+            width: 100%;
+        }
+ 
+        .btn-info.text-light:hover,
+        .btn-info.text-light:focus {
+            background: #000;
+        }
+        table, tbody, td, tfoot, th, thead, tr {
+            border-color: #ededed !important;
+            border-style: solid;
+            border-width: 1px !important;
+        }
+        .site-title{
+            color: rgb(165,16,18);
+        }
+        .active{
+            border-bottom: 3px solid rgb(164,15,17);
+            transition: .15s;
+        }
+        .custom-alert{
+            position: fixed;
+            top: 25px;
+            right: 25px;
+        }
+        .custom-bg{
+            background-color:  rgb(165,16,18);
+            border: 1px solid  rgb(165,16,18);
+        }
+        .custom-bg:hover{
+            background-color:  rgb(165,16,18,.85);
+            border: 1px solid  rgb(165,16,18)
+        }
+        .custom-alert{
+            position: fixed;
+            top: 80px;
+            right: 25px;
+        }
+        #dashboard-menu{
+            position: fixed;
+            height: 100%;
+            z-index: 11;
+        }
+
+        @media screen and (max-width: 991px){
+            #dashboard-menu{
+                height: auto;
+                width: 100%;
+            }
+            #main-content{
+                margin-top: 60px;
+            }
+        }
+    </style>
+    <?php 
+        require('includes/header.php');
+    ?>
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden" id="page-container">
+                
+                <div class="row">
+                    <div class="col-12">
+                        <div id="calendar"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    
+    <!-- Event Details Modal -->
+ 
+<?php 
+require("includes/dbConfig.php");
+$schedules = $con->query("SELECT * FROM `schedule_list`");
+$sched_res = [];
+foreach($schedules->fetch_all(MYSQLI_ASSOC) as $row){
+    $row['sdate'] = date("F d, Y",strtotime($row['start_datetime']));
+    $row['edate'] = date("h:i A",strtotime($row['end_datetime']));
+    $sched_res[$row['id']] = $row;
+}
+?>
+<?php 
+if(isset($con)) $con->close();
+?>
+
+<script>
+    var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
+</script>
+<script src="../scripts.js"></script>
+<?php require('includes/scripts.php');?>
+</body>
+</html>
